ff476914cb854bd57b6452b0041b8b5b12c6d7a1
Manas <saparbektv@gmail.com>
2026-08-11 15:51:57 +0500

    Format repositories in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,20 @@
 pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
 rootProject.name = "NinPlus"
+
 include(":app")
